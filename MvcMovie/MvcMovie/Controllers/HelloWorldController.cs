d9fbc0ebb7d67f19ffc2990c23cb901dcb0f462f
mcsergyu <sergiomachado@vodafone.pt>
2017-11-04 19:10:24 +0000

    Adding a view

--- MvcMovie/MvcMovie/Controllers/HelloWorldController.cs
+++ MvcMovie/MvcMovie/Controllers/HelloWorldController.cs
@@ -14,9 +14,9 @@ namespace MvcMovie.Controllers
         // 
         // GET: /HelloWorld/
 
-        public string Index()
+        public IActionResult Index()
         {
-            return "This is my default action...";
+            return View();
         }
 
         // 
